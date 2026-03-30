0d72199de84b8281fe512783c2aa0d509d80cb56
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-03-30 21:05:08 +0100

    vai

--- src/rastro/users/interfaces/presenters.py
+++ src/rastro/users/interfaces/presenters.py
@@ -5,20 +5,14 @@ from rastro.users.application.dtos import UserOutput
 
 
 class UserPublic(TypedDict):
-    id: int
     email: str
     username: str
-    is_active: bool
-    is_verified: bool
 
 
 class UserPresenter(Presenter[UserOutput, UserPublic]):
     @staticmethod
     def present(private: UserOutput) -> UserPublic:
         return UserPublic(
-            id=private.id,
             email=private.email,
             username=private.username,
-            is_active=private.is_active,
-            is_verified=private.is_verified,
         )
