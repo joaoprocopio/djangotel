3d4e1cc568f9a8b9ace195513229edaf8bf279ec
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-03-30 22:28:17 +0100

    vai

--- src/rastro/auth/domain/value_objects.py
+++ src/rastro/auth/domain/value_objects.py
@@ -1,10 +1,16 @@
+import unicodedata
+
 from rastro_base.value_object import ValueObject
 
 
-class Email(ValueObject[str]): ...
+class Email(ValueObject[str]):
+    def normalize(self) -> str:
+        return self.value.strip().lower()
 
 
-class Username(ValueObject[str]): ...
+class Username(ValueObject[str]):
+    def normalize(self) -> str:
+        return unicodedata.normalize("NFKC", self.value)
 
 
 class RawPassword(ValueObject[str]): ...
