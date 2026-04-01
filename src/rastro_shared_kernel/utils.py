753227a090f23b8293aab7dcb479dc3629d0c288
joaoprocopio <joaovitorcprocopio@gmail.com>
2026-04-02 00:40:15 +0100

    vai

--- src/rastro_shared_kernel/utils.py
+++ src/rastro_shared_kernel/utils.py
@@ -5,4 +5,6 @@ E = TypeVar("E", bound=StrEnum)
 
 
 def str_enum_to_choices(str_enum: type[E]) -> list[tuple[str, str]]:
-    return [(member.value, member.value.title()) for member in str_enum]
+    return [
+        (member.value, member.value.replace("_", " ").title()) for member in str_enum
+    ]
